e06b4720940ab713918f3d88105cf429e41a774f
Dario George <edu.dariogeorge21@gmail.com>
2025-06-02 15:24:09 +0530

    Deploy YouTube clone to GitHub Pages

--- src/pages/index.js
+++
@@ -1,4 +0,0 @@
-// Export all pages from a single file for easier imports
-export { default as Home } from './Home/Home';
-export { default as VideoPage } from './VideoPage/VideoPage';
-export { default as SearchPage } from './SearchPage/SearchPage';
